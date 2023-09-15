30c79be392a6d3ae6806eec3f805b41b443c2dbe
MoHyeokJin <ddubi1115@naver.com>
2023-09-15 13:20:36 +0900

    project_1

---
+++ project_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    int num;
+    int basken = 0;
+    int s[31];
+    bool turn = false;
+
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < 31; i++) {
+        s[i] = i+1;
+    }
+
+    while (basken < 32) {
+        cout << "개수를 입력하세요. : ";
+        cin >> num;
+        if(num < 4 && num > 0 && turn == false) {
+            cout << "사용자가 부른 숫자!" << endl;
+            for (int j = 0; j < num; j++) {
+                cout << s[basken + j] << endl;
+                if(s[basken + j] == 31) {
+                    cout << "게임 종료! 컴퓨터의 승리입니다." << endl;
+                    break;
+                }
+            }
+            basken = num + basken;
+            turn = true;
+        }
+        else {
+            cout << "1~3 사이의 숫자를 입력하세요." << endl;
+        }
+
+        if(turn == true) {
+            int c_num = rand() % 3 + 1;
+            cout << "컴퓨터가 부른 숫자!" << endl;
+            for (int x = 0; x < c_num; x++) {
+                cout << s[basken + x] << endl;
+                if(s[basken + x] == 31) {
+                    cout << "게임 종료! 사용자의 승리입니다." << endl;
+                    break;
+                }
+            }
+            basken = c_num + basken;
+            turn = false;
+        }
+    }
+
+
+
+
+
+}
