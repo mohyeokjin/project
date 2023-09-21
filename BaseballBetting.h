6db1797ce5dbafd8e892621d3ce7c2d41552fb5e
MoHyeokJin <ddubi1115@naver.com>
2023-09-21 19:33:30 +0900

    main functuin +, count +

--- BaseballBetting.h
+++ BaseballBetting.h
@@ -22,6 +22,26 @@ class BaseballBetting {
     void setbet_1(int bet_1) { this -> bet_1 = bet_1; }
     void setbet_2(int bet_2) { this -> bet_2 = bet_2; }
 
+    void Start() {
+        cout << "*****************************************************************" << endl;
+        cout << "*                                                               *" << endl;
+        cout << "*                         야구 베팅 게임                        *" << endl;
+        cout << "*                                                               *" << endl;
+        cout << "*                         < 게임 규칙 >                         *" << endl;
+        cout << "*                                                               *" << endl;
+        cout << "*          1. 플레이어 2명 진행                                 *" << endl;
+        cout << "*          2. 랜덤한 숫자 3개 맞추기                            *" << endl;
+        cout << "*          3. 같은 자리, 같은 숫자 = strike                     *" << endl;
+        cout << "*          4. 다른 자리, 같은 숫자 = ball                       *" << endl;
+        cout << "*          5. strike = 3점, ball = 1점                          *" << endl;
+        cout << "*          6. 숫자, 베팅 여부 및 베팅 금액 입력                 *" << endl;
+        cout << "*          7. 더 높은 점수의 플레이어가 베팅 금액 획득          *" << endl;
+        cout << "*          8. 점수 30점 또는 베팅 금액 소진 시 게임 종료        *" << endl;
+        cout << "*                                                               *" << endl;
+        cout << "*****************************************************************" << endl;
+        cout << endl << endl;
+    }
+
     void Score(int x, int y) {
         cout << "================== 점수 ===================" << endl;
         cout << "첫 번째 플레이어 : " << x << endl;
@@ -35,6 +55,18 @@ class BaseballBetting {
         cout << "첫 번째 플레이어 : " << score_1 << endl;
         cout << "두 번째 플레이어 : " << score_2 << endl;
         cout << "================== 금액 ===================" << endl;
+        cout << "첫 번째 플레이어 : " << bet_1 << endl;
+        cout << "두 번째 플레이어 : " << bet_2 << endl;
+        cout << endl;
+    }
+
+        void Fianl() {
+        cout << endl;
+        cout << "================ 최종 결과 =================" << endl;
+        cout << "================ 최종 점수 =================" << endl;
+        cout << "첫 번째 플레이어 : " << score_1 << endl;
+        cout << "두 번째 플레이어 : " << score_2 << endl;
+        cout << "================ 최종 금액 =================" << endl;
         cout << "첫 번째 플레이어 : " << bet_1 << endl;
         cout << "두 번째 플레이어 : " << bet_2 << endl;
         cout << endl;
--- project_5_dev.cpp
+++ project_5_dev.cpp
@@ -6,8 +6,6 @@
 using namespace std;
 
 int main() {
-    cout << "프로젝트. 야구 배팅 게임" << endl;
-
     srand((unsigned int)time(NULL));
     BaseballBetting game;
     int c_baseBall[3];
@@ -21,11 +19,10 @@ int main() {
     int count_1 = 0;
     int score_1 = 0;
     int score_2 = 0;
-    int a, b, x, y; // 각 플레이어 점수, 베팅 여부, 베팅 금액
+    int r, a, b, x, y; // 각 플레이어 점수, 베팅 여부, 베팅 금액
     int score_s = 3; // strike 점수
     int score_b = 1; // ball 점수
 
-    cout << "야구 게임" << endl;
     for(int i = 0; i < 3; i++) {
         int num = rand() % 9 + 1;
         c_baseBall[i] = num;
@@ -36,12 +33,15 @@ int main() {
         }
     }
     
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 3; i++) { // 게임 설명을 돕기 위해 랜덤 숫자 출력
         cout << c_baseBall[i] << " ";
     }
-    cout << endl;
+    cout << endl << endl;
+
+    game.Start();
 
     while(true) {
+        cout << endl;
         cout << "첫 번째 플레이어 : 1 ~ 9 사이의 숫자 3개를 입력해주세요. ";
         cin >> u_baseBall[0] >> u_baseBall[1] >> u_baseBall[2];
 
@@ -80,19 +80,19 @@ int main() {
         game.setscore_1(game.getscore_1()+score_1);
         game.setscore_2(game.getscore_2()+score_2);
 
-        cout << "베팅하시겠습니까? (1 : 수락, 2 : 거절) ";
+        cout << endl << "베팅하시겠습니까? (1 : 수락, 2 : 거절) ";
         cin >> a >> b;
 
         if(a == 1 && b == 1) { // 플레이어 두 명 모두 동의해야 베팅 진행
-            cout << "첫 번째 플레이어 : 베팅할 금액을 입력해주세요. (베팅 가능 금액 : " << game.getbet_1() << ") : ";
+            cout << endl << "첫 번째 플레이어 : 베팅할 금액을 입력해주세요. (베팅 가능 금액 : " << game.getbet_1() << ") : ";
             cin >> x;
             if(x > game.getbet_1()) {
                 cout << "베팅 가능 금액을 초과하였습니다. 다시 입력해주세요. " << endl;
                 cin >> x;
             }
-            cout << "두 번째 플레이어 : 베팅할 금액을 입력해주세요. (베팅 가능 금액 : " << game.getbet_2() << ") : ";
+            cout << endl << "두 번째 플레이어 : 베팅할 금액을 입력해주세요. (베팅 가능 금액 : " << game.getbet_2() << ") : ";
             cin >> y;
-            if(y > game.getbet_1()) {
+            if(y > game.getbet_2()) {
                 cout << "베팅 가능 금액을 초과하였습니다. 다시 입력해주세요. " << endl;
                 cin >> y;
             }
@@ -100,19 +100,25 @@ int main() {
             game.Score(score_1, score_2); // 베팅 후 점수 공개 및 결과 발표
 
             if(score_1 > score_2) {
-                cout << endl << "첫 번째 플레이어 승리!" << endl;
+                cout << endl << "첫 번째 플레이어 승리!" << endl << endl;
+                cout << "첫 번째 플레이어 - strike : " << strike << "      " << "ball : " << ball << endl;
+                cout << "두 번째 플레이어 - strike : " << strike_1 << "      " << "ball : " << ball_1 << endl;
                 game.setbet_1(game.getwin_1(game.getbet_1(), x, y));
                 game.setbet_2(game.getlose_2(game.getbet_2(), x, y));
                 game.Result();
             }
             else if(score_1 < score_2) {
-                cout << endl << "두 번째 플레이어 승리!" << endl;
+                cout << endl << "두 번째 플레이어 승리!" << endl << endl;
+                cout << "첫 번째 플레이어 - strike : " << strike << "      " << "ball : " << ball << endl;
+                cout << "두 번째 플레이어 - strike : " << strike_1 << "      " << "ball : " << ball_1 << endl;
                 game.setbet_1(game.getlose_1(game.getbet_1(), x, y));
                 game.setbet_2(game.getwin_2(game.getbet_2(), x, y));
                 game.Result();
             }
             else if(score_1 == score_2) {
                 cout << endl << "무승부입니다!" << endl << endl;
+                cout << "첫 번째 플레이어 - strike : " << strike << "      " << "ball : " << ball << endl;
+                cout << "두 번째 플레이어 - strike : " << strike_1 << "      " << "ball : " << ball_1 << endl;
                 game.Result();
             }
         }
@@ -125,18 +131,19 @@ int main() {
             if(game.getbet_1() == game.getbet_2()) {
                 cout << "================== 결과 ===================" << endl;
                 cout << "무승부입니다!" << endl;
+                game.Fianl();
                 break;
             }
             else if(game.getbet_1() > game.getbet_2()) {
                 cout << "================== 결과 ===================" << endl << endl;
-                cout << "첫 번째 플레이어 승리!" << endl;
-                game.Result();
+                cout << "첫 번째 플레이어가 최종 승리하였습니다." << endl;
+                game.Fianl();
                 break;
             }
             else if(game.getbet_1() < game.getbet_2()) {
                 cout << "================== 결과 ===================" << endl << endl;
-                cout << "두 번째 플레이어 승리!" << endl;
-                game.Result();
+                cout << "두 번째 플레이어가 최종 승리하였습니다." << endl;
+                game.Fianl();
                 break;
             }
         }
@@ -144,13 +151,13 @@ int main() {
             if(game.getscore_1() > game.getscore_2() || game.getbet_1() > game.getbet_2()) {
                 cout << "===========================================" << endl << endl;
                 cout << "첫 번째 플레이어가 최종 승리하였습니다." << endl;
-                game.Result();
+                game.Fianl();
                 break;
             }
             else if(game.getscore_1() < game.getscore_2() || game.getbet_1() < game.getbet_2()) {
                 cout << "===========================================" << endl << endl;
                 cout << "두 번째 플레이어가 최종 승리하였습니다." << endl;
-                game.Result();
+                game.Fianl();
                 break;
             }
         }
